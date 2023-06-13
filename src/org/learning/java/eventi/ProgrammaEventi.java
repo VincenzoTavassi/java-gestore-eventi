64b63ba208f81a2137b7767408c750f276d98b04
VincenzoTavassi <info@egadivacanze.it>
2023-06-13 15:00:42 +0200

    milestone 4

---
+++ src/org/learning/java/eventi/ProgrammaEventi.java
@@ -0,0 +1,44 @@
+package org.learning.java.eventi;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammaEventi {
+
+    private String title;
+    private List<Event> eventList;
+
+    public ProgrammaEventi(String title, List<Event> eventList) {
+        this.title = title;
+        this.eventList = new ArrayList<>(eventList);
+    }
+
+    public void addEvent (Event event) {
+        eventList.add(event);
+    }
+
+    public List<Event> listEventsByDate(LocalDate date) {
+        List<Event> filteredList = null;
+        for (Event event : eventList) {
+            if (event.getDate() == date) filteredList.add(event);
+        }
+        return filteredList;
+    }
+
+    public int countEvents() {
+        return eventList.size();
+    }
+
+    public void resetEvents() {
+        eventList = null;
+    }
+
+    public String listEvents() {
+        String list = null;
+    for (Event event : eventList) {
+        list += event.getDate() + " - " + event.getTitle() + "\n";
+    }
+    return list;
+    }
+}
